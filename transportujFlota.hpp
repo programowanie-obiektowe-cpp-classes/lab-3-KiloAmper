badd6dee71115645bfca7de87d45a191e93cb721
Bartek Skoniecki <b.skoniecki@interia.pl>
2024-11-13 23:10:50 +0100

    lab3_zejsciowka

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,31 @@
 #pragma once
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+        return 0; // jeœli towar to 0, nie twórz statków
+
+    unsigned int przetransportowano = 0;
+    unsigned int liczbaZaglowcow    = 0;
+    Stocznia     stocznia;
+
+    while (przetransportowano < towar) {
+        Statek*      statek              = stocznia();
+        unsigned int transportowanaIlosc = statek->transportuj();
+        przetransportowano += transportowanaIlosc;
+
+        // SprawdŸ, czy statek jest typu Zaglowiec
+        if (dynamic_cast< Zaglowiec* >(statek) != nullptr) {
+            liczbaZaglowcow++;
+        }
+
+        delete statek; // Usuñ dynamicznie utworzony statek
+    }
+
+    return liczbaZaglowcow;
 }
